1dff1ed8cf26a33fffe253eeb05a94c25d7f7dea
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-01-09 11:06:53 +0300

    Created Database and Migrations

---
+++ DungeonsDragons/GameLogic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameLogic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
